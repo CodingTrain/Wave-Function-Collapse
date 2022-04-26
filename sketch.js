eda953b72cbe28f879dec268f6daa607191977b9
15Nova22 <15-22novacom@web.de>
2022-04-26 16:12:13 +0200

    Added Soldermask
    The Soldermask can be colored in common PCB-Colors

--- sketch.js
+++ sketch.js
@@ -14,21 +14,22 @@ function preload() {
 
 function setup() {
   createCanvas(800, 800);
+  const solderMaskColor = 'blue'; //one of black | white | red | yellow | green | blue | none(default)
 
   // Loaded and created the tiles
-  tiles[0] = new Tile(tileImages[0], ["AAA", "AAA", "AAA", "AAA"]);
-  tiles[1] = new Tile(tileImages[1], ["BBB", "BBB", "BBB", "BBB"]);
-  tiles[2] = new Tile(tileImages[2], ["BBB", "BCB", "BBB", "BBB"]);
-  tiles[3] = new Tile(tileImages[3], ["BBB", "BDB", "BBB", "BDB"]);
-  tiles[4] = new Tile(tileImages[4], ["ABB", "BCB", "BBA", "AAA"]);
-  tiles[5] = new Tile(tileImages[5], ["ABB", "BBB", "BBB", "BBA"]);
-  tiles[6] = new Tile(tileImages[6], ["BBB", "BCB", "BBB", "BCB"]);
-  tiles[7] = new Tile(tileImages[7], ["BDB", "BCB", "BDB", "BCB"]);
-  tiles[8] = new Tile(tileImages[8], ["BDB", "BBB", "BCB", "BBB"]);
-  tiles[9] = new Tile(tileImages[9], ["BCB", "BCB", "BBB", "BCB"]);
-  tiles[10] = new Tile(tileImages[10], ["BCB", "BCB", "BCB", "BCB"]);
-  tiles[11] = new Tile(tileImages[11], ["BCB", "BCB", "BBB", "BBB"]);
-  tiles[12] = new Tile(tileImages[12], ["BBB", "BCB", "BBB", "BCB"]);
+  tiles[0] = new Tile(tileImages[0], ["AAA", "AAA", "AAA", "AAA"], solderMaskColor);
+  tiles[1] = new Tile(tileImages[1], ["BBB", "BBB", "BBB", "BBB"], solderMaskColor);
+  tiles[2] = new Tile(tileImages[2], ["BBB", "BCB", "BBB", "BBB"], solderMaskColor);
+  tiles[3] = new Tile(tileImages[3], ["BBB", "BDB", "BBB", "BDB"], solderMaskColor);
+  tiles[4] = new Tile(tileImages[4], ["ABB", "BCB", "BBA", "AAA"], solderMaskColor);
+  tiles[5] = new Tile(tileImages[5], ["ABB", "BBB", "BBB", "BBA"], solderMaskColor);
+  tiles[6] = new Tile(tileImages[6], ["BBB", "BCB", "BBB", "BCB"], solderMaskColor);
+  tiles[7] = new Tile(tileImages[7], ["BDB", "BCB", "BDB", "BCB"], solderMaskColor);
+  tiles[8] = new Tile(tileImages[8], ["BDB", "BBB", "BCB", "BBB"], solderMaskColor);
+  tiles[9] = new Tile(tileImages[9], ["BCB", "BCB", "BBB", "BCB"], solderMaskColor);
+  tiles[10] = new Tile(tileImages[10], ["BCB", "BCB", "BCB", "BCB"], solderMaskColor);
+  tiles[11] = new Tile(tileImages[11], ["BCB", "BCB", "BBB", "BBB"], solderMaskColor);
+  tiles[12] = new Tile(tileImages[12], ["BBB", "BCB", "BBB", "BCB"], solderMaskColor);
 
   for (let i = 2; i < 14; i++) {
     for (let j = 1; j < 4; j++) {
--- tile.js
+++ tile.js
@@ -9,13 +9,91 @@ function compareEdge(a, b) {
 }
 
 class Tile {
-  constructor(img, edges) {
+  constructor(img, edges, solderMaskColor = 'none') {
     this.img = img;
     this.edges = edges;
     this.up = [];
     this.right = [];
     this.down = [];
     this.left = [];
+    if(solderMaskColor !== 'none') {
+      this.changeColor(solderMaskColor);
+    }
+  }
+
+  /**
+   * Changes the Color of the Sodlermask of the Tiles
+   *
+   * @param color The input color as string
+   * @returns void
+   */
+  changeColor(color) {
+    let cArray = this.getColorAsArray(color);
+    if(!cArray) return;
+    let copper = [141, 73, 49];
+    let alpha = 0.5;
+
+    this.img.loadPixels();
+    for(let i = 0; i < 4 * (this.img.width * this.img.height); i+= 4) {
+      if(
+        this.img.pixels[i]   == 5   /* R-Value of solder in circuit tiles */ &&
+        this.img.pixels[i+1] == 105 /* G-Value of solder in circuit tiles */ &&
+        this.img.pixels[i+2] == 10  /* B-Value of solder in circuit tiles */
+      ) {
+        this.img.pixels[i]   = cArray[0];
+        this.img.pixels[i+1] = cArray[1];
+        this.img.pixels[i+2] = cArray[2];
+      } else if(
+        this.img.pixels[i]   == 0   /* R-Value of conductor track in circuit tiles */ &&
+        this.img.pixels[i+1] == 208 /* G-Value of conductor track in circuit tiles */ &&
+        this.img.pixels[i+2] == 122 /* B-Value of conductor track in circuit tiles */
+      ) {
+        this.img.pixels[i]   = this.blendColors(copper[0], cArray[0], alpha);
+        this.img.pixels[i+1] = this.blendColors(copper[1], cArray[1], alpha);
+        this.img.pixels[i+2] = this.blendColors(copper[2], cArray[2], alpha);
+      }
+    }
+    this.img.updatePixels();
+  }
+
+  /**
+   *  Blends between 2 colors
+   *
+   * @param baseColor  The background color
+   * @param blendColor The color do add on top
+   * @param amount     The amount of the color to add (1 = only blendColor; 0 = only baseColor)
+   * @returns
+   */
+  blendColors(baseColor, blendColor, amount) {
+    return ((blendColor * amount) + (1-amount) * baseColor);
+  }
+
+  /**
+   * Colors based on the Electronic color codes and corosponding RAL colors
+   *
+   * @see https://en.wikipedia.org/wiki/Electronic_color_code#Color_band_system
+   * @see https://www.ralcolorchart.com/
+   *
+   * @param color The Inputcolor as string
+   *
+   * @returns array | undefined
+   */
+  getColorAsArray(color) {
+    switch(color) {
+      case 'black':
+        return [14, 14, 16];
+      case 'white':
+        return [227, 217, 198];
+      case 'red':
+        return [167, 41, 32];
+      case 'yellow':
+        return [246, 182, 0];
+      case 'green':
+        return [97, 153, 59];
+      case 'blue':
+        return [0, 124, 176];
+    }
+    return undefined;
   }
 
   analyze(tiles) {
