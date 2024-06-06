da7b6e9f8a9fb8ef01d24e208790a812cfd27a04
Daniel Shiffman <daniel.shiffman@gmail.com>
2024-06-06 16:17:49 -0400

    it woiks

--- overlapping-model/sketch.js
+++ overlapping-model/sketch.js
@@ -2,7 +2,9 @@ let img;
 let tiles = [];
 let grid = [];
 const tileSize = 3;
-const DIM = 25;
+const DIM = 50;
+
+let toggle = false;
 
 function preload() {
   img = loadImage('flowers.png');
@@ -34,7 +36,68 @@ function startOver() {
 }
 
 function draw() {
+  // draw all the extracted tiles
   background(0);
+  // showAllTiles();
+  wfc();
+
+  // let t = random(tiles);
+  // let w = 20;
+  // renderTile(t.img, 0, 0, w, w);
+
+  // // compare t to all other tiles right horizontal edge
+  // let x = w;
+  // let y = 0;
+  // for (let i = 0; i < tiles.length; i++) {
+  //   let other = tiles[i];
+  //   if (compareVEdge(t.img, other.img, 1, 0)) {
+  //     renderTile(other.img, x, y, w, w);
+  //     y += w;
+  //     if (y > height) {
+  //       y = 0;
+  //       x += w;
+  //     }
+  //   }
+  // }
+
+  // noLoop();
+}
+
+function showAllTiles() {
+  let x = 0;
+  let y = 0;
+  const w = width / DIM;
+  const h = height / DIM;
+
+  for (let i = 0; i < tiles.length; i++) {
+    render(tiles[i].img, x, y, w, h);
+    x += w;
+    if (x > width) {
+      x = 0;
+      y += h;
+    }
+  }
+
+  translate(0, height / 2);
+  // draw the original image with each pixel w,h
+  img.loadPixels();
+  let pw = w / 3;
+  let ph = h / 3;
+
+  for (let i = 0; i < img.width; i++) {
+    for (let j = 0; j < img.height; j++) {
+      let index = (i + j * img.width) * 4;
+      const r = img.pixels[index + 0];
+      const g = img.pixels[index + 1];
+      const b = img.pixels[index + 2];
+      fill(r, g, b);
+      noStroke();
+      rect(i * pw, j * ph, pw, ph);
+    }
+  }
+}
+
+function wfc() {
   const w = width / DIM;
   const h = height / DIM;
   for (let j = 0; j < DIM; j++) {
@@ -42,7 +105,11 @@ function draw() {
       let cell = grid[i + j * DIM];
       if (cell.collapsed) {
         let index = cell.options[0];
-        render(tiles[index].img, i * w, j * h, w, h);
+        if (mouseIsPressed) {
+          renderTile(tiles[index].img, i * w, j * h, w, h);
+        } else {
+          renderCenter(tiles[index].img, i * w, j * h, w, h);
+        }
       } else {
         noFill();
         stroke(51);
@@ -96,7 +163,7 @@ function draw() {
           if (neighbor.collapsed) {
             const nState = neighbor.options[0];
             for (const option of options) {
-              if (compareHEdge(tiles[option].img, tiles[nState].img, 0, 2)) {
+              if (compareHEdge(tiles[option].img, tiles[nState].img, 1, 0)) {
                 validOptions.push(option);
               }
             }
@@ -115,7 +182,7 @@ function draw() {
             }
             let tempValidOptions = [];
             for (const option of possibleOptions) {
-              if (compareHEdge(tiles[option].img, tiles[nState].img, 2, 0)) {
+              if (compareHEdge(tiles[option].img, tiles[nState].img, 0, 1)) {
                 tempValidOptions.push(option);
               }
             }
@@ -136,7 +203,7 @@ function draw() {
             }
             let tempValidOptions = [];
             for (const option of possibleOptions) {
-              if (compareVEdge(tiles[option].img, tiles[nState].img, 0, 2)) {
+              if (compareVEdge(tiles[option].img, tiles[nState].img, 0, 1)) {
                 tempValidOptions.push(option);
               }
             }
@@ -157,7 +224,7 @@ function draw() {
             }
             let tempValidOptions = [];
             for (const option of possibleOptions) {
-              if (compareVEdge(tiles[option].img, tiles[nState].img, 2, 0)) {
+              if (compareVEdge(tiles[option].img, tiles[nState].img, 1, 0)) {
                 tempValidOptions.push(option);
               }
             }
@@ -179,18 +246,23 @@ function compareHEdge(a, b, aEdge, bEdge) {
   a.loadPixels();
   b.loadPixels();
   let same = true;
-  for (let i = 0; i < tileSize; i++) {
-    let aIndex = (aEdge + i * tileSize) * 4;
-    let bIndex = (bEdge + i * tileSize) * 4;
-    let ar = a.pixels[aIndex + 0];
-    let br = b.pixels[bIndex + 0];
-    let ag = a.pixels[aIndex + 1];
-    let bg = b.pixels[bIndex + 1];
-    let ab = a.pixels[aIndex + 2];
-    let bb = b.pixels[bIndex + 2];
-    if (ar != br || ag != bg || ab != bb) {
-      same = false;
-      break;
+
+  for (j = 0; j < 2; j++) {
+    for (let i = 0; i < tileSize; i++) {
+      let colA = aEdge + (j % tileSize);
+      let colB = bEdge + (j % tileSize);
+      let aIndex = (colA + i * tileSize) * 4;
+      let bIndex = (colB + i * tileSize) * 4;
+      let ar = a.pixels[aIndex + 0];
+      let br = b.pixels[bIndex + 0];
+      let ag = a.pixels[aIndex + 1];
+      let bg = b.pixels[bIndex + 1];
+      let ab = a.pixels[aIndex + 2];
+      let bb = b.pixels[bIndex + 2];
+      if (ar != br || ag != bg || ab != bb) {
+        same = false;
+        break;
+      }
     }
   }
   return same;
@@ -200,24 +272,45 @@ function compareVEdge(a, b, aEdge, bEdge) {
   a.loadPixels();
   b.loadPixels();
   let same = true;
-  for (let i = 0; i < tileSize; i++) {
-    let aIndex = (i + aEdge * tileSize) * 4;
-    let bIndex = (i + bEdge * tileSize) * 4;
-    let ar = a.pixels[aIndex + 0];
-    let br = b.pixels[bIndex + 0];
-    let ag = a.pixels[aIndex + 1];
-    let bg = b.pixels[bIndex + 1];
-    let ab = a.pixels[aIndex + 2];
-    let bb = b.pixels[bIndex + 2];
-    if (ar != br || ag != bg || ab != bb) {
-      same = false;
-      break;
+  for (j = 0; j < 2; j++) {
+    for (let i = 0; i < tileSize; i++) {
+      let rowA = aEdge + (j % tileSize);
+      let rowB = bEdge + (j % tileSize);
+      let aIndex = (i + rowA * tileSize) * 4;
+      let bIndex = (i + rowB * tileSize) * 4;
+      let ar = a.pixels[aIndex + 0];
+      let br = b.pixels[bIndex + 0];
+      let ag = a.pixels[aIndex + 1];
+      let bg = b.pixels[bIndex + 1];
+      let ab = a.pixels[aIndex + 2];
+      let bb = b.pixels[bIndex + 2];
+      if (ar != br || ag != bg || ab != bb) {
+        same = false;
+        break;
+      }
     }
   }
   return same;
 }
 
-function render(pattern, x, y, w, h) {
+function renderCenter(pattern, x, y, w, h) {
+  pattern.loadPixels();
+  // hardcoding center pixel
+  let i = 1;
+  let j = 1;
+  let index = (i + j * tileSize) * 4;
+  const r = pattern.pixels[index + 0];
+  const g = pattern.pixels[index + 1];
+  const b = pattern.pixels[index + 2];
+  fill(r, g, b);
+  noStroke();
+  rect(x, y, w, h);
+  // noFill();
+  // stroke(255, 0, 0);
+  // rect(x, y, w, h);
+}
+
+function renderTile(pattern, x, y, w, h) {
   pattern.loadPixels();
   for (let i = 0; i < tileSize; i++) {
     for (let j = 0; j < tileSize; j++) {
@@ -230,7 +323,7 @@ function render(pattern, x, y, w, h) {
       rect(x + (i * w) / tileSize, y + (j * h) / tileSize, w / tileSize, h / tileSize);
     }
   }
-  noFill();
-  stroke(255, 0, 0);
-  rect(x, y, w, h);
+  // noFill();
+  // stroke(255, 0, 0);
+  // rect(x, y, w, h);
 }
