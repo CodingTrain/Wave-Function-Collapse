bf210d01bd9ed7fbc617d7b1840edf9357bee41f
Sergey Alyaev <saly@norceresearch.no>
2025-04-29 16:14:53 +0200

    Changed to a queue system

--- p5js/overlapping-model/sketch.js
+++ p5js/overlapping-model/sketch.js
@@ -7,19 +7,21 @@ let grid;
 
 // Refactored variables names
 // Number of cells along one dimension of the grid
-let GRID_SIZE = 100;
+let GRID_SIZE = 400;
 // Maximum depth for recursive checking of cells
-let MAX_RECURSION_DEPTH = 1000;
+let MAX_RECURSION_DEPTH = 1000000000;
+const REDUCTIONS_PER_FRAME = 10000;
 // Size of each tile (3x3 by default)
 let TILE_SIZE = 3;
 let w;
+let reductionQueue = [];
 
 // Turn on or off rotations and reflections
 const ROTATIONS = false;
 const REFLECTIONS = false;
 
 function preload() {
-  sourceImage = loadImage('images/flowers.png');
+  sourceImage = loadImage('images/Flowers.png');
 }
 
 function setup() {
@@ -36,17 +38,17 @@ function setup() {
   // Create the grid
   initializeGrid();
 
-  // Perform initial wave function collapse step
-  wfc();
+  // // Perform initial wave function collapse step
+  // wfc();
 
-  // The WFC function only collapses one cell at a time
-  // This extra bit collapses any other cells that can be
-  for (let cell of grid) {
-    if (cell.options.length == 1) {
-      cell.collapsed = true;
-      reduceEntropy(grid, cell, 0);
-    }
-  }
+  // // The WFC function only collapses one cell at a time
+  // // This extra bit collapses any other cells that can be
+  // for (let cell of grid) {
+  //   if (cell.options.length == 1) {
+  //     cell.collapsed = true;
+  //     reduceEntropyOnce(grid, cell, 0);
+  //   }
+  // }
   // add pause checkbox
   let pauseCheckbox = createCheckbox('Pause', false);
   pauseCheckbox.changed(() => {
@@ -91,71 +93,111 @@ function draw() {
 
 // The Wave Function Collapse algorithm
 function wfc() {
-  // Calculate entropy for each cell
-  for (let cell of grid) {
-    cell.calculateEntropy();
-  }
+  if (reductionQueue.length == 0) {
+    // Calculate entropy for each cell
+    for (let cell of grid) {
+      cell.calculateEntropy();
+    }
 
-  // Find cells with the lowest entropy (simplified as fewest options left)
-  // Thie refactored method to find the lowest entropy cells avoids sorting
-  let minEntropy = Infinity;
-  let lowestEntropyCells = [];
+    // Find cells with the lowest entropy (simplified as fewest options left)
+    // Thie refactored method to find the lowest entropy cells avoids sorting
+    let minEntropy = Infinity;
+    let lowestEntropyCells = [];
 
-  for (let cell of grid) {
-    if (!cell.collapsed) {
-      if (cell.entropy < minEntropy) {
-        minEntropy = cell.entropy;
-        lowestEntropyCells = [cell];
-      } else if (cell.entropy === minEntropy) {
-        lowestEntropyCells.push(cell);
+    for (let cell of grid) {
+      if (!cell.collapsed) {
+        if (cell.entropy < minEntropy) {
+          minEntropy = cell.entropy;
+          lowestEntropyCells = [cell];
+        } else if (cell.entropy === minEntropy) {
+          lowestEntropyCells.push(cell);
+        }
       }
     }
-  }
 
-  // We're done if all cells are collapsed!
-  if (lowestEntropyCells.length == 0) {
-    noLoop();
-    return;
-  }
-
-  // Randomly select one of the lowest entropy cells to collapse
-  const cell = random(lowestEntropyCells);
-  cell.collapsed = true;
-
-  // Need to redraw this cell
-  cell.needsRedraw = true;
-
-  // Choose one option randomly from the cell's options
-  const pick = random(cell.options);
-
-  // If there are no possible tiles that fit there!
-  if (pick == undefined) {
-    console.log('ran into a conflict');
-    initializeGrid();
-    return;
-  }
-
-  // Set the final tile
-  cell.options = [pick];
-
-  // Propagate entropy reduction to neighbors
-  reduceEntropy(grid, cell, 0);
-
-  // Collapse anything that can be!
-  for (let cell of grid) {
-    if (cell.options.length == 1) {
-      cell.collapsed = true;
-      reduceEntropy(grid, cell, 0);
+    // We're done if all cells are collapsed!
+    if (lowestEntropyCells.length == 0) {
+      noLoop();
+      return;
     }
+
+    // Randomly select one of the lowest entropy cells to collapse
+    const cell = random(lowestEntropyCells);
+    cell.collapsed = true;
+
+    // Need to redraw this cell
+    cell.needsRedraw = true;
+
+    // Choose one option randomly from the cell's options
+    const pick = random(cell.options);
+
+    // If there are no possible tiles that fit there!
+    if (pick == undefined) {
+      console.log('ran into a conflict');
+      initializeGrid();
+      return;
+    }
+
+    // Changing logic to gradually reduce entropy
+
+    // Set the final tile
+    cell.options = [pick];
+
+    // add to queue
+    addToQueue(reductionQueue, cell, 0);
+  }
+  else {
+    // Propagate entropy reduction to neighbors
+    let reductionCount = 0;
+    while (reductionQueue.length > 0) {
+      reduceEntropyOnce(grid, reductionQueue);
+      reductionCount++;
+      if (reductionCount > REDUCTIONS_PER_FRAME) {
+        break;
+      }
+    }
+
+    // // Collapse anything that can be!
+    // for (let cell of grid) {
+    //   if (cell.options.length == 1) {
+    //     cell.collapsed = true;
+    //     addToQueue(reductionQueue, cell, 0);
+    //   }
+    // }
   }
 }
 
-function reduceEntropy(grid, cell, depth) {
+function addToQueue(cellDepthQueueArray, cell, depth) {
+  // Check if the cell is already in the queue
+  for (let i = 0; i < cellDepthQueueArray.length; i++) {
+    if (cellDepthQueueArray[i].cell.index == cell.index) {
+      return;
+    }
+  }
+
+  // Add the cell and depth to the queue
+  cellDepthQueueArray.push({
+    cell: cell,
+    depth: depth
+  });
+}
+
+
+
+function reduceEntropyOnce(grid, cellDepthQueueArray) {
+  cellDepth = cellDepthQueueArray.shift();
+  let cell = cellDepth.cell;
+  let depth = cellDepth.depth;
+
   // Stop propagation if max depth is reached or cell already checked
-  if (depth > MAX_RECURSION_DEPTH) return;
+  if (depth > MAX_RECURSION_DEPTH) return "Recursion limit reached";
 
   // Mark cell as checked
   cell.checked = true;
+
+  if (cell.options.length == 1) {
+    cell.collapsed = true;
+  }
 
   // Need to redraw this cell
   cell.needsRedraw = true;
@@ -164,41 +206,53 @@ function reduceEntropy(grid, cell, depth) {
   let i = floor(index % GRID_SIZE);
   let j = floor(index / GRID_SIZE);
 
+  let needsPropogation = 0;
+
   // Update neighboring cells based on adjacency rules
   // RIGHT
   if (i + 1 < GRID_SIZE) {
     let rightCell = grid[i + 1 + j * GRID_SIZE];
-    if (checkOptions(cell, rightCell, EAST)) {
-      reduceEntropy(grid, rightCell, depth + 1);
+    if (checkOptionsReduced(cell, rightCell, EAST)) {
+      addToQueue(cellDepthQueueArray, rightCell, depth + 1);
+      needsPropogation++;
     }
   }
 
   // LEFT
   if (i - 1 >= 0) {
     let leftCell = grid[i - 1 + j * GRID_SIZE];
-    if (checkOptions(cell, leftCell, WEST)) {
-      reduceEntropy(grid, leftCell, depth + 1);
+    if (checkOptionsReduced(cell, leftCell, WEST)) {
+      addToQueue(cellDepthQueueArray, leftCell, depth + 1);
+      needsPropogation++;
     }
   }
 
   // DOWN
   if (j + 1 < GRID_SIZE) {
     let downCell = grid[i + (j + 1) * GRID_SIZE];
-    if (checkOptions(cell, downCell, SOUTH)) {
-      reduceEntropy(grid, downCell, depth + 1);
+    if (checkOptionsReduced(cell, downCell, SOUTH)) {
+      addToQueue(cellDepthQueueArray, downCell, depth + 1);
+      needsPropogation++;
     }
   }
 
   // UP
   if (j - 1 >= 0) {
     let upCell = grid[i + (j - 1) * GRID_SIZE];
-    if (checkOptions(cell, upCell, NORTH)) {
-      reduceEntropy(grid, upCell, depth + 1);
+    if (checkOptionsReduced(cell, upCell, NORTH)) {
+      addToQueue(cellDepthQueueArray, upCell, depth + 1);
+      needsPropogation++;
     }
+  }
+
+  if (needsPropogation > 0) {
+    return "Need to reduce entropy";
+  } else {
+    return "Entropy reduced";
   }
 }
 
-function checkOptions(cell, neighbor, direction) {
+function checkOptionsReduced(cell, neighbor, direction) {
   // Check if the neighbor is valid and not already collapsed
   if (neighbor && !neighbor.collapsed) {
     // Collect valid options based on the current cell's adjacency rules
@@ -213,7 +267,7 @@ function checkOptions(cell, neighbor, direction) {
 
     if (neighbor.options.length < oldOptLength) {
       return true;
-    }else{
+    } else {
       return false;
     }
   } else {
